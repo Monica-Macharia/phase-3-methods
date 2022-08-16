fe7b201db48690c4259cf3710b86470de5b6daee
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-08-16 14:11:17 +0300

    commit tree 498f8a34632e20f0a73fca2facb6ce75b19375bf

--- methods.rb
+++ methods.rb
@@ -1,1 +1,21 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet("Naureen")
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+end
+def add(num1, num2)
+    return num1 + num2
+end
+def halve(int)
+    if int.class == Integer
+        return int/2
+    end
+    return nil
+end
